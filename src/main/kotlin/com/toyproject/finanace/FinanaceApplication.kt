6033dce1f321b69b066386093cf4fdbb30ba159f
chanmin01 <68585373+chanmin01@users.noreply.github.com>
2024-11-04 23:11:08 +0900

    chore: 프로젝트 초기 설정 및 기본 환경 구성
    - .gitattributes 및 .gitignore 추가
    - Gradle Wrapper 및 빌드 설정 파일 추가
    - application.properties 파일을 통해 기본 설정 적용
    - settings.gradle.kts 및 주요 경로 설정
    - 기본 애플리케이션 파일(FinanceApplication.kt) 및 테스트 파일 추가
    
    프로젝트의 기본 환경을 설정하여 빌드와 실행 준비 완료.

---
+++ src/main/kotlin/com/toyproject/finanace/FinanaceApplication.kt
@@ -0,0 +1,11 @@
+package com.toyproject.finanace
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinanaceApplication
+
+fun main(args: Array<String>) {
+    runApplication<FinanaceApplication>(*args)
+}
---
+++ src/test/kotlin/com/toyproject/finanace/FinanaceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toyproject.finanace
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinanaceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
