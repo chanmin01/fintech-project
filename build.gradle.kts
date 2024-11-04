7bbb7cfc78c031601285b4c7d70f12c333c0aad5
chanmin01 <68585373+chanmin01@users.noreply.github.com>
2024-11-04 22:44:48 +0900

    chore: Kotlin 및 Spring Boot 버전 업데이트 및 멀티 모듈 설정 완료
    - Kotlin 버전 1.9.25, Spring Boot 버전 3.3.5로 업데이트
    - 멀티 모듈 구성 적용: 각 모듈에 필요한 플러그인과 의존성 설정
    - JDK 17을 타겟으로 Kotlin 컴파일 옵션 설정 (jvmTarget = 17)
    - JUnit 5 플랫폼 사용으로 테스트 환경 설정
    - 공통 의존성으로 Spring Boot Starter 및 Kotlin 라이브러리 추가

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25" apply false
+    kotlin("plugin.jpa") version "1.6.0" apply false
+
+    id("org.springframework.boot") version "3.3.5" apply false
+    id("io.spring.dependency-management") version "1.1.6" apply false
+}
+
+repositories {
+    mavenCentral()
+}
+
+allprojects {
+    group = "com.toyproject"
+
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects {
+    apply {
+        plugin("org.jetbrains.kotlin.jvm")
+        plugin("org.jetbrains.kotlin.plugin.spring")
+
+        plugin("org.springframework.boot")
+        plugin("io.spring.dependency-management")
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "17"
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+    dependencies {
+        implementation("org.springframework.boot:spring-boot-starter")
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+        implementation("org.springframework.boot:spring-boot-starter-test")
+
+    }
+}
