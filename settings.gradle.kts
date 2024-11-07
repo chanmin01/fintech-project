8bcde61bd2d09b04c79e4c3b556aa2435c40e5d6
chanmin01 <68585373+chanmin01@users.noreply.github.com>
2024-11-07 16:01:44 +0900

    chore: 프로젝트 설정 업데이트 및 모듈 포함
    - 루트 프로젝트 이름을 `finanace`로 설정
    - `api`, `consumer`, `gateway`, `domain` 모듈 포함
    - `api:src:main:css` 모듈을 `css`로 이름 설정
    - `css` 모듈 추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,8 @@
 rootProject.name = "finanace"
+include("api")
+include("consumer")
+include("gateway")
+include("api:src:main:css")
+findProject(":api:src:main:css")?.name = "css"
+include("css")
+include("domain")
